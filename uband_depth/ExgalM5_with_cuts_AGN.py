cfcd63a6a683533d9673cbd9697a79df7304841a
Roberto Assef <roberto.assef@mail.udp.cl>
2021-02-03 20:51:55 +0000

    Corrected bug in metric with filter definition and re-ran everything.

--- uband_depth/ExgalM5_with_cuts_AGN.py
+++ uband_depth/ExgalM5_with_cuts_AGN.py
@@ -6,8 +6,8 @@ from lsst.sims.maf.metrics import BaseMetric
 
 class ExgalM5_with_cuts_AGN(BaseMetric):
     
-    def __init__(self, m5Col='fiveSigmaDepth', extinction_cut=1.0, \
-                 metricName='ExgalM5_with_cuts_AGN', **kwargs):
+    def __init__(self, lsstFilter, m5Col='fiveSigmaDepth', units='mag', extinction_cut=1.0, \
+                 filterCol='filter', metricName='ExgalM5_with_cuts_AGN', **kwargs):
             
         #Dust Extinction limit to which consider regions. If left unconstrained,
         #it ends up finding extremely shallow (m_lim=100) 5 sigma regions. Not
@@ -15,17 +15,23 @@ class ExgalM5_with_cuts_AGN(BaseMetric):
         #metric of DESC.
         self.extinction_cut = extinction_cut
 
+        #Save the filter information.
+        self.filterCol = filterCol
+        self.lsstFilter = lsstFilter
+
         #This calculation is reliant on the ExgalM5 metric. So declare that here.
-        self.exgalM5 = ExgalM5()
+        self.exgalM5 = ExgalM5(m5Col=m5Col, units=units, lsstFilter=self.lsstFilter)
         
         #Initiate the metric.
         super(ExgalM5_with_cuts_AGN, self).__init__(
-            col=m5Col, metricName=metricName, maps=self.exgalM5.maps, **kwargs)
+            col=[m5Col, filterCol], metricName=metricName, maps=self.exgalM5.maps, units=units,
+            **kwargs)
         
     def run(self, dataSlice, slicePoint=None):
         # exclude areas with high extinction
         if slicePoint['ebv'] > self.extinction_cut:
             return self.badval
         
-        mlim5 = self.exgalM5.run(dataSlice, slicePoint)
+        dS = dataSlice[dataSlice[self.filterCol] == self.lsstFilter]
+        mlim5 = self.exgalM5.run(dS, slicePoint)
         return mlim5
