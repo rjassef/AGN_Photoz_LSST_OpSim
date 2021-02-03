477bc790fc7b428347c3f081ebe3c5dc242d7385
rjassef <roberto.assef@mail.udp.cl>
2021-02-02 22:10:27 -0300

    Module to get quasar colors from Matthew Temple's models.

---
+++ quasar_colors/quasar_colors.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import numpy as np
+from astropy.table import Table
+import os
+import re
+
+def Temple_colors(color,z,imag,ebv=0):
+
+    #We only have two reddening values.
+    if ebv==0:
+        ebvname = ""
+    elif ebv==0.1:
+        ebvname = "_ebv01"
+    else:
+        print("Invalid reddening value.")
+        return None
+
+    #Get the path to this module. Necessary so that we can read the model files.
+    module_directory = os.path.dirname(os.path.abspath(__file__))
+
+    #Folder where we find the model files.
+    model_root = module_directory+"/Matthew_Temple_Models/redshift_colours_imag"
+
+    #First, try to see if the specific selection of magnitude and reddening exist already.
+    try:
+        fname = model_root+"{0:.0f}".format(imag*10)
+        fname += ebvname+".dat"
+        tab = Table.read(fname, format='ascii')
+        zs = tab['Redshift']
+        colors = tab[color]
+    except FileNotFoundError:
+        print("Invalid i-band magnitude")
+        return None
+
+
+    #Now, interpolate to get the right redshift.
+    return np.interp(z,zs,colors)
