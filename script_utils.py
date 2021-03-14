739b1b07b70fb14b86435b62de4d5d319c77d3df
Roberto Assef <roberto.assef@mail.udp.cl>
2021-03-14 20:00:27 +0000

    Added OpSim run areas to median tables.

--- script_utils.py
+++ script_utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+import astropy.units as u
+
 import sys 
 sys.path.append("/home/idies/workspace/Storage/rjassef/persistent/LSST_OpSim")
 from Scripts_NBs.opsimUtils import *
@@ -19,3 +22,13 @@ def find_completed_runs(n_metrics, resultDbPath, metricDataPath, bundleDicts=Non
         if len(list(bundleDicts[key].keys()))==n_metrics:
             completed_runs.append(key)
     return completed_runs
+
+def get_opsim_areas(key, bds):
+    runs = list(bds.keys())
+    area = np.zeros(len(runs))
+    for k, run in enumerate(runs):
+        mb = bds[run][key]
+        pix_area = (mb.slicer.pixArea * u.sr).to(u.deg**2).value
+        mask = mb.metricValues.mask
+        area[k] = len(mask[~mask])*pix_area
+    return area
